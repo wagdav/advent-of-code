ee6279d83987ca273d78a580366f65f5d46343e3
David Wagner <wagdav@gmail.com>
2024-12-01 06:22:46 +0100

    Solve day01

--- src/aoc2024/day01.clj
+++ src/aoc2024/day01.clj
@@ -1,10 +1,26 @@
 (ns aoc2024.day01
   (:require [clojure.string :as str]))
 
+(def example "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
+
+(defn transpose [v]
+  (apply mapv vector v))
+
 (defn parse-input [input]
   (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+       (mapv parse-long)
+       (partition 2)
+       (transpose)))
 
-(defn solve-part1 [input])
+(defn solve-part1 [[left right]]
+  (apply + (for [[l r] (map vector (sort left) (sort right))]
+             (abs (- l r)))))
 
-(defn solve-part2 [input])
+(defn solve-part2 [[left right]]
+  (let [f (frequencies right)]
+    (apply + (for [l left] (* l (get f l 0))))))
--- test/aoc2024/day01_test.clj
+++ test/aoc2024/day01_test.clj
@@ -3,14 +3,19 @@
             [aoc2024.day01 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 11 (solve-part1 (parse-input example-input))))
+    (is (= 31 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day01.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 2164381 (solve-part1 input)))
+      (is (= 20719933 (solve-part2 input))))))
