632799d9a53cd62586922a1de3b1c6a0f35ba703
David Wagner <wagdav@gmail.com>
2024-12-02 06:51:22 +0100

    Solve day02

--- src/aoc2024/day02.clj
+++ src/aoc2024/day02.clj
@@ -2,9 +2,25 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (for [line (str/split-lines input)]
+    (map parse-long (re-seq #"-?\d+" line))))
 
-(defn solve-part1 [input])
+(defn safe? [report]
+  (and (or (apply < report)
+           (apply > report))
+       (every?
+         (fn [[a b]]
+           (<= 1 (abs (- b a)) 3))
+         (partition 2 1 report))))
 
-(defn solve-part2 [input])
+(defn solve-part1 [input]
+  (count (filter safe? input)))
+
+(defn dampen [report]
+  (for [i (range (count report))]
+    (concat (take i report) (drop (inc i) report))))
+
+(defn solve-part2 [input]
+  (count (filter #(or (safe? %)
+                      (some safe? (dampen %)))
+                 input)))
--- test/aoc2024/day02_test.clj
+++ test/aoc2024/day02_test.clj
@@ -3,14 +3,20 @@
             [aoc2024.day02 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 2 (solve-part1 (parse-input example-input))))
+    (is (= 4 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day02.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 526 (solve-part1 input)))
+      (is (= 566 (solve-part2 input))))))
