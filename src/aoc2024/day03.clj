0c5408951c0de997c387ddb35a01f40e37bc35f0
David Wagner <wagdav@gmail.com>
2024-12-03 07:51:02 +0100

    Solve day03

--- src/aoc2024/day03.clj
+++ src/aoc2024/day03.clj
@@ -2,9 +2,28 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (flatten
+    (interpose :do
+      (for [m (str/split input #"do\(\)")]
+        (interpose :dont (str/split m #"don't\(\)"))))))
 
-(defn solve-part1 [input])
+(defn muls [p]
+  (for [[_ a b] (re-seq #"mul\((\d+),(\d+)\)" p)]
+    (* (parse-long a) (parse-long b))))
 
-(defn solve-part2 [input])
+(defn solve-part1 [input]
+  (apply + (mapcat muls (filter string? input))))
+
+(defn solve-part2 [input]
+  (apply +
+    (second
+      (reduce
+        (fn [[enabled res] p]
+          (case p
+            :do   [true res]
+            :dont [false res]
+            [enabled (if enabled
+                       (into res (muls p))
+                       res)]))
+        [true []]
+        input))))
--- test/aoc2024/day03_test.clj
+++ test/aoc2024/day03_test.clj
@@ -3,14 +3,15 @@
             [aoc2024.day03 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example1 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+(def example2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 161 (solve-part1 (parse-input example1))))
+    (is (= 48 (solve-part2 (parse-input example2)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day03.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 168539636 (solve-part1 input)))
+      (is (= 97529391 (solve-part2 input))))))
