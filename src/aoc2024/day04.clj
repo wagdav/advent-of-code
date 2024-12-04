0ae13c592faa6878e659f112575fb193a9ee32c4
David Wagner <wagdav@gmail.com>
2024-12-04 08:09:42 +0100

    Solve day04

--- src/aoc2024/day04.clj
+++ src/aoc2024/day04.clj
@@ -1,10 +1,63 @@
 (ns aoc2024.day04
   (:require [clojure.string :as str]))
 
+(def example-input "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX")
+
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (mapv #(apply vector %)
+    (for [r (str/split-lines input)]
+      (for [c r]
+        c))))
 
-(defn solve-part1 [input])
+(def directions [[0 1] [0 -1] [1 0] [-1 0] [1 1] [-1 -1] [-1 1] [1 -1]])
 
-(defn solve-part2 [input])
+(defn word [p [r c] [dr dc]]
+  (apply str
+    (for [i (range 4)
+          :let [l (get-in p [(+ r (* i dr))
+                             (+ c (* i dc))])]
+          :while l]
+      l)))
+
+(defn words [puzzle]
+  (let [rows (count puzzle)
+        cols (count (first puzzle))]
+    (for [r (range rows)
+          c (range cols)
+          d directions]
+      (word puzzle [r c] d))))
+
+(defn cross [p [r c] [dr dc]]
+  (apply str
+    (for [pos [[(- r dr) (- c dc)]
+               [   r        c]
+               [(+ r dr) (+ c dc)]]
+          :let [l (get-in p pos)]
+          :while l]
+      l)))
+
+(defn crosses [puzzle]
+  (let [rows (count puzzle)
+        cols (count (first puzzle))]
+    (for [r (range rows)
+          c (range cols)]
+      (for [d [[1 1] [-1 -1] [1 -1] [-1 1]]]
+        (cross puzzle [r c] d)))))
+
+(defn solve-part1 [input]
+  (count (filter #(= "XMAS" %) (words input))))
+
+(defn solve-part2 [input]
+  (->> (crosses input)
+       (map frequencies)
+       (filter #(= 2 (get % "MAS")))
+       (count)))
--- test/aoc2024/day04_test.clj
+++ test/aoc2024/day04_test.clj
@@ -3,14 +3,23 @@
             [aoc2024.day04 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 18 (solve-part1 (parse-input example-input))))
+    (is (= 9 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day04.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 2462 (solve-part1 input)))
+      (is (= 1877 (solve-part2 input))))))
