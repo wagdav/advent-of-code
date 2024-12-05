5a3c41f590ac0906685942a1a6925bf12d5ba105
David Wagner <wagdav@gmail.com>
2024-12-05 07:03:41 +0100

    Solve day05

--- src/aoc2024/day05.clj
+++ src/aoc2024/day05.clj
@@ -1,10 +1,43 @@
 (ns aoc2024.day05
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as s]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [[rules updates] (str/split input #"\n\n")]
+    {:rules
+     (update-vals
+       (->> (re-seq #"\d+" rules)
+            (map parse-long)
+            (partition 2)
+            (group-by first))
+       (fn [v] (set (map second v))))
+     :updates (->> (for [line (str/split-lines updates)]
+                     (map parse-long (re-seq #"\d+" line))))}))
 
-(defn solve-part1 [input])
+(defn correct? [rules pages]
+  (let [p (first pages)
+        r (rest pages)]
+    (if (seq r)
+      (if (empty? (s/difference (set r) (rules p)))
+        (recur rules (rest pages))
+        false)
+      true)))
 
-(defn solve-part2 [input])
+(defn middle [s]
+  (first (drop (quot (count s) 2) s)))
+
+(defn reorder [rules pages]
+  (sort #(contains? (rules %1) %2) pages))
+
+(defn solve-part1 [{:keys [rules updates]}]
+  (->> updates
+       (filter (partial correct? rules))
+       (map middle)
+       (apply +)))
+
+(defn solve-part2 [{:keys [rules updates]}]
+  (->> updates
+       (remove (partial correct? rules))
+       (map (partial reorder rules))
+       (map middle)
+       (apply +)))
--- test/aoc2024/day05_test.clj
+++ test/aoc2024/day05_test.clj
@@ -3,14 +3,42 @@
             [aoc2024.day05 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 143 (solve-part1 (parse-input example-input))))
+    (is (= 123 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
-    (let [input (parse-input (slurp (io/resource "day05.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+    (let [input (parse-input (slurp "resources/day05.txt"))]
+      (is (= 5991 (solve-part1 input)))
+      (is (= 5479 (solve-part2 input))))))
