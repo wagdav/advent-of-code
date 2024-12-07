2aae1905fcb4aee9eb71a3afc7f7bc6c42b990d5
David Wagner <wagdav@gmail.com>
2024-12-07 09:32:35 +0100

    Solve day07

--- src/aoc2024/day07.clj
+++ src/aoc2024/day07.clj
@@ -2,9 +2,31 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (for [line (str/split-lines input)]
+    (let [xs (->> (re-seq #"-?\d+" line)
+                  (map parse-long))]
+      [(first xs) (rest xs)])))
 
-(defn solve-part1 [input])
+(defn eval-with [xs ops]
+  (reduce
+    (fn [result x]
+      (for [r result
+            op ops]
+        (op r x)))
+    (take 1 xs)
+    (drop 1 xs)))
 
-(defn solve-part2 [input])
+(defn conkat [a b] (parse-long (str a b)))
+
+(defn calibrate [ops [res xs]]
+  (if (some #(= res %) (eval-with xs ops)) res 0))
+
+(defn solve-part1 [input]
+  (->> input
+       (map (partial calibrate [+ *]))
+       (apply +)))
+
+(defn solve-part2 [input]
+  (->> input
+       (map (partial calibrate [+ * conkat]))
+       (apply +)))
--- test/aoc2024/day07_test.clj
+++ test/aoc2024/day07_test.clj
@@ -3,14 +3,23 @@
             [aoc2024.day07 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 3749 (solve-part1 (parse-input example-input))))
+    (is (= 11387 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day07.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 14711933466277 (solve-part1 input)))
+      (is (= 286580387663654 (solve-part2 input))))))
