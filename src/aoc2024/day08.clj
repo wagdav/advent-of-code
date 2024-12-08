51c12645c60461834c657dda422cfee4fd09faf9
David Wagner <wagdav@gmail.com>
2024-12-08 10:01:15 +0100

    Solve day08

--- src/aoc2024/day08.clj
+++ src/aoc2024/day08.clj
@@ -2,9 +2,42 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [rows (str/split-lines input)]
+    {:size (count rows)
+     :antennas (apply merge-with into
+                 (for [[i row] (map-indexed vector rows)
+                       [j a]   (map-indexed vector row)
+                       :when (not= a \.)]
+                   {a [[i j]]}))}))
 
-(defn solve-part1 [input])
+(defn in-bounds [size]
+  (fn [[x y]]
+    (and (< -1 x size) (< -1 y size))))
 
-(defn solve-part2 [input])
+(defn antinodes1 [size]
+  (fn [a b]
+    (filter (in-bounds size)
+      (let [diff (mapv - b a)]
+        [(mapv - a diff) (mapv + b diff)]))))
+
+(defn antinodes2 [size]
+  (fn [a b]
+    (let [diff (mapv - b a)]
+      (concat
+        (take-while (in-bounds size) (iterate #(mapv + % diff) a))
+        (take-while (in-bounds size) (iterate #(mapv - % diff) b))))))
+
+(defn search [antennas antinode-fn]
+  (into #{}
+    (for [[_ ps] antennas
+          p1 ps
+          p2 ps
+          :when (not= p1 p2)
+          an (antinode-fn p1 p2)]
+      an)))
+
+(defn solve-part1 [{:keys [size antennas]}]
+  (count (search antennas (antinodes1 size))))
+
+(defn solve-part2 [{:keys [size antennas]}]
+  (count (search antennas (antinodes2 size))))
