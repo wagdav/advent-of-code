2e93271cdb443629519965cd37d5a7991f84cbe2
David Wagner <wagdav@gmail.com>
2024-12-10 08:09:32 +0100

    Solve day10

--- src/aoc2024/day10.clj
+++ src/aoc2024/day10.clj
@@ -2,9 +2,54 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (vec
+    (for [line (str/split-lines input)]
+      (mapv parse-long (re-seq #"\d" line)))))
 
-(defn solve-part1 [input])
+(defn trailheads [m]
+  (for [[i row] (map-indexed vector m)
+        [j h] (map-indexed vector row)
+        :when (= h 0)]
+    [i j]))
 
-(defn solve-part2 [input])
+(defn actions [m p]
+  (let [c (get-in m p)]
+    (for [d [[-1 0] [1 0] [0 1] [0 -1]]
+          :let [n (mapv + p d)]
+          :when (= (inc c) (get-in m n))]
+      n)))
+
+(defn score1 [m start]
+  (loop [ends 0, q (list start), explored #{}]
+    (if (seq q)
+      (let [cur (peek q)
+            q' (into (pop q)
+                     (remove explored (actions m cur)))]
+        (recur
+          (if (= 9 (get-in m cur)) (inc ends) ends)
+          q' (conj explored cur)))
+      ends)))
+
+(defn score2 [m start]
+  (loop [paths #{}, q (list [start]), explored #{}]
+    (if (seq q)
+      (let [cur (peek q)
+            q' (into (pop q)
+                     (->> (actions m (last cur))
+                          (remove explored)
+                          (map #(conj cur %))))]
+        (recur
+          (if (= 9 (get-in m (last cur))) (conj paths cur) paths)
+          q'
+          (conj explored cur)))
+     (count paths))))
+
+(defn solve-part1 [input]
+  (apply +
+    (for [start (trailheads input)]
+      (score1 input start))))
+
+(defn solve-part2 [input]
+  (apply +
+    (for [start (trailheads input)]
+      (score2 input start))))
--- test/aoc2024/day10_test.clj
+++ test/aoc2024/day10_test.clj
@@ -3,14 +3,21 @@
             [aoc2024.day10 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 36 (solve-part1 (parse-input example-input))))
+    (is (= 81 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day10.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 574 (solve-part1 input)))
+      (is (= 1238 (solve-part2 input))))))
