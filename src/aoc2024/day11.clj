3443ce1763971e8e090f603044f53516c1775bd2
David Wagner <wagdav@gmail.com>
2024-12-11 08:14:40 +0100

    Solve day11

--- src/aoc2024/day11.clj
+++ src/aoc2024/day11.clj
@@ -1,10 +1,52 @@
-(ns aoc2024.day11
-  (:require [clojure.string :as str]))
+(ns aoc2024.day11)
 
 (defn parse-input [input]
   (->> (re-seq #"-?\d+" input)
        (mapv parse-long)))
 
-(defn solve-part1 [input])
+(defn splitable? [n]
+  (even? (count (str n))))
 
-(defn solve-part2 [input])
+(defn split [n]
+  (let [as-string (str n)
+        length (count as-string)
+        half (quot length 2)]
+    (map parse-long [(.substring as-string 0 half)
+                     (.substring as-string half length)])))
+
+(defn blink [stones]
+  (mapcat
+    (fn [stone]
+        (cond
+          (= stone 0)        [1]
+          (splitable? stone) (split stone)
+          :else              [(* stone 2024)]))
+    stones))
+
+(defn solve-part1 [input]
+   (count (nth (iterate blink input) 25)))
+
+(defn blink2 [stones]
+  (merge-with +
+    {1 (get stones 0 0)}
+    (apply merge-with +
+      (for [[stone amount] stones
+            :when (splitable? stone)
+            :let [[a b] (split stone)]]
+        (merge-with + {a amount} {b amount})))
+    (into {}
+      (for [[stone amount] stones
+            :when (and (not= stone 0)
+                       (not (splitable? stone)))]
+        [(* 2024 stone) amount]))))
+
+(defn solve-part2 [input]
+  (let [stone-map (into {} (map vector input (repeat 1)))
+        blinks 75]
+    (->> (nth (iterate blink2 stone-map) blinks)
+         (vals)
+         (apply +))))
+
+(comment
+  (time (solve-part1 (parse-input (slurp "resources/day11.txt"))))
+  (time (solve-part2 (parse-input (slurp "resources/day11.txt")))))
--- test/aoc2024/day11_test.clj
+++ test/aoc2024/day11_test.clj
@@ -3,14 +3,14 @@
             [aoc2024.day11 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "125 17")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 55312 (solve-part1 (parse-input example-input))))
+    (is (= 65601038650482 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day11.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 220999 (solve-part1 input)))
+      (is (= 261936432123724 (solve-part2 input))))))
