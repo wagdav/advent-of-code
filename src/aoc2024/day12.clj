4ae3c5738e80afc833ce16aaee598caed325ae5f
David Wagner <wagdav@gmail.com>
2024-12-12 08:19:12 +0100

    Solve day12

--- src/aoc2024/day12.clj
+++ src/aoc2024/day12.clj
@@ -2,9 +2,75 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (into {}
+    (for [[i line] (map-indexed vector (str/split-lines input))
+          [j c] (map-indexed vector line)]
+     [[i j] c])))
 
-(defn solve-part1 [input])
+(defn garden [m start]
+  (let [plant (get m start)]
+    (loop [frontier #{start}, visited #{start}]
+      (if (seq frontier)
+        (let [frontier' (for [f frontier
+                              d [[-1 0] [1 0] [0 -1] [0 1]]
+                              :let [pos' (mapv + f d)
+                                    plant' (get m pos')]
+                              :when (and (= plant plant')
+                                         (not (visited pos')))]
+                          pos')]
+          (recur (set frontier') (into visited frontier)))
+        visited))))
 
-(defn solve-part2 [input])
+(defn perimeter [garden]
+  (for [g garden
+        d [[-1 0] [1 0] [0 -1] [0 1]]
+        :when (nil? (garden (mapv + g d)))]
+    g))
+
+(defn gardens [input]
+  (loop [gs [], coords input]
+    (if (seq coords)
+      (let [g (garden input (ffirst coords))]
+        (recur
+          (conj gs g)
+          (apply dissoc coords g)))
+      gs)))
+
+(defn border [garden]
+  (for [g garden
+        d [[-1 0] [1 0] [0 -1] [0 1]]
+        :let [pos' (mapv + g d)]
+        :when (not (contains? garden pos'))]
+    [pos' d]))
+
+(defn collapse [border d]
+  (loop [border border]
+    (let [border' (remove
+                    (fn [[b n]]
+                      (let [moved (mapv + b d)]
+                        (contains? (set border) [moved n])))
+                    border)]
+      (if (= border border')
+        border
+        (recur border')))))
+
+(defn sides [garden]
+  (-> (border garden)
+      (collapse [0 1])
+      (collapse [1 0])
+      (count)))
+
+(defn solve-part1 [input]
+  (apply +
+    (for [g (gardens input)]
+      (* (count g) (count (perimeter g))))))
+
+(defn solve-part2 [input]
+  (apply +
+    (for [g (gardens input)]
+      (* (count g) (sides g)))))
+
+(comment
+  (time (solve-part1 (parse-input (slurp "resources/day12.txt")))) ; 1477924
+  (time (solve-part2 (parse-input (slurp "resources/day12.txt"))))) ; 841934
+
--- test/aoc2024/day12_test.clj
+++ test/aoc2024/day12_test.clj
@@ -3,14 +3,23 @@
             [aoc2024.day12 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 1930 (solve-part1 (parse-input example-input))))
+    (is (= 1206 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day12.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 1477924 (solve-part1 input)))
+      (is (= 841934 (solve-part2 input))))))
