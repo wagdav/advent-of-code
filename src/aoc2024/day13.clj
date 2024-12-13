bbfb9a1bd36d72950a767a4cfddb123c7e2b35e4
David Wagner <wagdav@gmail.com>
2024-12-13 11:30:13 +0100

    Solve day13

--- src/aoc2024/day13.clj
+++ src/aoc2024/day13.clj
@@ -2,9 +2,31 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (for [block (str/split input #"\R\R")]
+    (let [[a b prize] (->> (re-seq #"\d+" block)
+                           (mapv parse-long)
+                           (partition 2))]
+      {:a a, :b b, :prize prize})))
 
-(defn solve-part1 [input])
+; To reach the prize means: alpha * a + beta * b = p, where a and b are the
+; button vectors, p is the location of the prize. These form two linear
+; equations for alpha and beta.
+(defn tokens
+  ([machine]
+   (tokens 0 machine))
+  ([unit {:keys [a b prize]}]
+   (let [[a1 a2] a
+         [b1 b2] b
+         [p1 p2] (mapv (partial + unit) prize)
+         alpha (/ (- p1 (* p2 (/ b1 b2)))
+                  (- a1 (* a2 (/ b1 b2))))
+         beta (/ (- p2 (* alpha a2)) b2)]
+     (if (and (integer? alpha) (integer? beta))
+       (+ (* 3 alpha) beta)
+       0))))
 
-(defn solve-part2 [input])
+(defn solve-part1 [input]
+  (apply + (map tokens input)))
+
+(defn solve-part2 [input]
+  (apply + (map (partial tokens 10000000000000) input)))
--- test/aoc2024/day13_test.clj
+++ test/aoc2024/day13_test.clj
@@ -3,14 +3,28 @@
             [aoc2024.day13 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 480 (solve-part1 (parse-input example-input))))
+    (is (= 875318608908 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day13.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 29201 (solve-part1 input)))
+      (is (= 104140871044942 (solve-part2 input))))))
