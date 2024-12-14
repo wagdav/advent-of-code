0615da3dc5eac38f024377e1a9320a4f3f385421
David Wagner <wagdav@gmail.com>
2024-12-14 10:44:16 +0100

    Solve day14

--- src/aoc2024/day14.clj
+++ src/aoc2024/day14.clj
@@ -3,8 +3,59 @@
 
 (defn parse-input [input]
   (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+       (mapv parse-long)
+       (partition 4)))
 
-(defn solve-part1 [input])
+(defn move [[sx sy] [x y vx vy]]
+  [(-> x (+ vx) (mod sx)) (-> y (+ vy) (mod sy)) vx vy])
 
-(defn solve-part2 [input])
+(defn move-all [size robots]
+  (map (partial move size) robots))
+
+(defn safety-factor [[sx sy] robots]
+  (apply *
+    (for [op1 [< >]
+          op2 [< >]]
+      (->> robots
+           (filter (fn [[x y _ _]] (and (op1 x (quot sx 2))
+                                        (op2 y (quot sy 2)))))
+           (count)))))
+
+(defn solve-part1
+ ([input]
+  (solve-part1 input [101 103]))
+ ([input size]
+  (safety-factor
+    size
+    (nth
+      (iterate (partial move-all size) input)
+      100))))
+
+(defn 🎄 [input]
+  (loop [i 1, robots input]
+    (let [robots' (move-all [101 103] robots)
+          m (->> robots'
+                 (map #(take 2 %))
+                 (into #{}))]
+      (if (= (count m) (count robots'))
+        [i m]
+        (recur (inc i) robots')))))
+
+(defn solve-part2 [input]
+  (first (🎄 input)))
+
+(defn viz [[sx sy] m]
+  (doseq [y (range sy)]
+    (doseq [x (range sx)]
+      (if (m [x y])
+        (print "#")
+        (print " ")))
+    (println)))
+
+; clj -X aoc2024.day14/run
+(defn run [opts]
+  (let [[i p] (🎄 (parse-input (slurp "resources/day14.txt")))]
+    (viz [101 103] p)))
+
+(comment
+  (time (solve-part1 (parse-input (slurp "resources/day14.txt")))))
--- test/aoc2024/day14_test.clj
+++ test/aoc2024/day14_test.clj
@@ -3,14 +3,25 @@
             [aoc2024.day14 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input ")
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 21 (solve-part1 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day14.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 228410028 (solve-part1 input)))
+      (is (= 8258 (solve-part2 input))))))
