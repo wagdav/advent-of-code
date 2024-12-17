ac8aaeef05539d049132e7206cf1b2c635bee4f6
David Wagner <wagdav@gmail.com>
2024-12-17 08:36:34 +0100

    Solve day16 part1

--- src/aoc2024/day17.clj
+++ src/aoc2024/day17.clj
@@ -1,10 +1,102 @@
 (ns aoc2024.day17
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math :refer [pow]]))
+
+(def example-input "Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0")
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [[registers ops] (->> (re-seq #"-?\d+" input)
+                             (mapv parse-long)
+                             (split-at 3))]
+    {:reg (into {} (map vector [:a :b :c] registers))
+     :program (into [] ops)
+     :out []
+     :pc 0}))
 
-(defn solve-part1 [input])
+(def ex (parse-input example-input))
 
-(defn solve-part2 [input])
+(defn combo [{:keys [reg] :as state} v]
+  (if (<= 0 v 3)
+    v
+    (case v
+      4 (reg :a)
+      5 (reg :b)
+      6 (reg :c))))
+
+(defn step [{:keys [pc reg program out] :as state}]
+    (if (>= pc (count program))
+      :halt
+      (let [op (program pc)
+            arg (program (inc pc))]
+        (case op
+          ;adv
+          0 (-> state
+                (assoc-in [:reg :a] (quot (reg :a) (int (pow 2 (combo state arg)))))
+                (assoc :pc (+ pc 2)))
+          ;bxl
+          1 (-> state
+                (assoc-in [:reg :b] (bit-xor (reg :b) arg))
+                (assoc :pc (+ pc 2)))
+          ;bst
+          2 (-> state
+                (assoc-in [:reg :b] (mod (combo state arg) 8))
+                (assoc :pc (+ pc 2)))
+          ;jnz
+          3 (if (zero? (reg :a))
+              (assoc state :pc (+ pc 2))
+              (assoc state :pc arg))
+          ; bxc
+          4 (-> state
+                (assoc-in [:reg :b] (bit-xor (reg :b) (reg :c)))
+                (assoc :pc (+ pc 2)))
+          ; out
+          5 (-> state
+                (update :out conj (mod (combo state arg) 8))
+                (assoc :pc (+ pc 2)))
+          ;bdv
+          6 (-> state
+                (assoc-in [:reg :b] (quot (reg :a) (int (pow 2 (combo state arg)))))
+                (assoc :pc (+ pc 2)))
+          ;cdv
+          7 (-> state
+                (assoc-in [:reg :c] (quot (reg :a) (int (pow 2 (combo state arg)))))
+                (assoc :pc (+ pc 2)))))))
+
+(defn run [input]
+  (loop [state input]
+     (let [state' (step state)]
+       (if (= state' :halt)
+         state
+         (recur state')))))
+
+(defn solve-part1 [input]
+  (str/join "," (:out (run input))))
+
+(defn solve-part2 [{:keys [program] :as input}]
+  (loop [reg-a 0]
+    (when (zero? (mod reg-a 10000))
+      (println reg-a))
+    (if (= program (:out (run (assoc-in input [:reg :a] reg-a))))
+      (do
+        (println "part2" reg-a)
+        reg-a)
+      (recur (inc reg-a)))))
+
+(def quine "Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0")
+
+(:out (run (assoc-in (parse-input quine) [:reg :a] 117440)))
+
+(defn run-part2 [opts] ; > 23620000
+  (solve-part2 (parse-input (slurp "resources/day17.txt"))))
+
+(defn run-part2-ex [opts]
+  (solve-part2 (parse-input quine)))
+
--- test/aoc2024/day17_test.clj
+++ test/aoc2024/day17_test.clj
@@ -3,14 +3,24 @@
             [aoc2024.day17 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0")
+
+(def example-input-quine "Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= "4,6,3,5,6,3,5,2,1,0" (solve-part1 (parse-input example-input))))
+    (is (= 117440 (solve-part2 (parse-input example-input-quine)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day17.txt")))]
-      (is (nil? (solve-part1 input)))
+      (is (= "4,1,7,6,4,1,0,2,7" (solve-part1 input)))
       (is (nil? (solve-part2 input))))))
