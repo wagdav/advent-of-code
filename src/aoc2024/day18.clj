526934c5a62cca22a5692320733022ea0bdd6086
David Wagner <wagdav@gmail.com>
2024-12-18 08:24:26 +0100

    Solve day18

--- src/aoc2024/day18.clj
+++ src/aoc2024/day18.clj
@@ -1,10 +1,47 @@
 (ns aoc2024.day18
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [aoc2024.search :as search]))
 
 (defn parse-input [input]
   (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+       (mapv parse-long)
+       (partitionv 2)))
 
-(defn solve-part1 [input])
+(defn steps [input size fallen]
+  (let [corrupted (into #{} (take fallen input))]
+    (:path-cost
+      (search/uniform-cost
+        (reify search/Problem
+          (actions [_ state]
+            (for [d [[-1 0] [1 0] [0 -1] [0 1]]
+                  :let [[x y] (mapv + state d)]
+                  :when (and (<= 0 x size)
+                             (<= 0 y size)
+                             (not (corrupted [x y])))]
+              [x y]))
+          (goal? [_ state]
+            (= state [size size]))
+          (initial-state [_]
+            [0 0])
+          (result [_ state action]
+            action)
+          (step-cost [_ state action]
+            1))))))
 
-(defn solve-part2 [input])
+(defn blocker-byte [input size]
+  (loop [i (count input)]
+    (if (steps input size i)
+      (nth input i)
+      (recur (dec i)))))
+
+(defn solve-part1
+  ([input]
+   (solve-part1 input 70 1024))
+  ([input size fallen]
+   (steps input size fallen)))
+
+(defn solve-part2
+  ([input]
+   (solve-part2 input 70))
+  ([input size]
+   (str/join "," (blocker-byte input size))))
--- test/aoc2024/day18_test.clj
+++ test/aoc2024/day18_test.clj
@@ -3,14 +3,38 @@
             [aoc2024.day18 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 22 (solve-part1 (parse-input example-input) 6 12)))
+    (is (= "6,1" (solve-part2 (parse-input example-input) 6))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day18.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 294 (solve-part1 input)))
+      (is (= "31,22" (solve-part2 input))))))
