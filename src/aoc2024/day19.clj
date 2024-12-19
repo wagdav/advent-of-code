eedd6d666c37783979d3d83655c976e332d90bd3
David Wagner <wagdav@gmail.com>
2024-12-19 08:10:06 +0100

    Solve day19

--- src/aoc2024/day19.clj
+++ src/aoc2024/day19.clj
@@ -2,9 +2,35 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [[a b] (str/split input #"\R\R")]
+    [(str/split a #", ")
+     (str/split-lines b)]))
 
-(defn solve-part1 [input])
+(defn prefix-matches [patterns design]
+  (for [p patterns
+        :when (str/starts-with? design p)]
+    (.substring design (count p))))
 
-(defn solve-part2 [input])
+(defn possible? [patterns design]
+  (loop [q (list design)]
+    (if (empty? q)
+      false
+      (let [d (peek q)]
+        (if (empty? d)
+          true
+          (recur (into (pop q) (prefix-matches patterns d))))))))
+
+(def arrangements
+  (memoize
+    (fn [patterns design]
+      (let [match (group-by empty? (prefix-matches patterns design))]
+         (+ (count (match true))
+            (apply +
+              (for [g (match false)]
+                (arrangements patterns g))))))))
+
+(defn solve-part1 [[patterns designs]]
+  (count (filter (partial possible? patterns) designs)))
+
+(defn solve-part2 [[patterns designs]]
+  (apply + (map (partial arrangements patterns) designs)))
--- test/aoc2024/day19_test.clj
+++ test/aoc2024/day19_test.clj
@@ -3,14 +3,23 @@
             [aoc2024.day19 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 6 (solve-part1 (parse-input example-input))))
+    (is (= 16 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day19.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 315 (solve-part1 input)))
+      (is (= 625108891232249 (solve-part2 input))))))
