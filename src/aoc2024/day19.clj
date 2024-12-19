ea4934ee4deef847ffaed1219eadbafa0094b1a4
David Wagner <wagdav@gmail.com>
2024-12-19 08:10:06 +0100

    --wip-- [skip ci]

--- src/aoc2024/day19.clj
+++ src/aoc2024/day19.clj
@@ -1,10 +1,38 @@
 (ns aoc2024.day19
   (:require [clojure.string :as str]))
 
-(defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+(def example-input "r, wr, b, g, bwu, rb, gb, br
 
-(defn solve-part1 [input])
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb")
+
+(defn parse-input [input]
+  (let [[a b] (str/split input #"\R\R")]
+    [(str/split a #", ")
+     (str/split-lines b)]))
+
+(def ex (parse-input example-input))
+
+(defn possible? [patterns design]
+  (loop [q (list design)]
+    (if (empty? q)
+      false
+      (let [d (peek q)]
+        (if (empty? d)
+          true
+          (recur (into (pop q) (for [p patterns
+                                     :when (str/starts-with? d p)]
+                                 (.substring d (count p))))))))))
+
+(defn solve-part1 [[patterns designs]]
+  (count (filter (partial possible? patterns) designs)))
+
+(solve-part1 ex)
 
 (defn solve-part2 [input])
