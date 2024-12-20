f80bb4f6145aa4631e7534b285796eba042a394a
David Wagner <wagdav@gmail.com>
2024-12-20 09:07:17 +0100

    Solve day20

--- src/aoc2024/day20.clj
+++ src/aoc2024/day20.clj
@@ -1,10 +1,52 @@
 (ns aoc2024.day20
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [aoc2024.search :as search]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (apply merge-with into
+    (for [[i row] (map-indexed vector (str/split-lines input))
+          [j c]   (map-indexed vector row)
+          :when (not= c \#)]
+      (case c
+        \. {:free #{[i j]}}
+        \S {:free #{[i j]}
+            :pos [i j]}
+        \E {:free #{[i j]}
+            :end [i j]}))))
 
-(defn solve-part1 [input])
+(defn fastest-path [input]
+  (mapv :pos
+    (:path
+      (search/uniform-cost
+        (reify search/Problem
+          (initial-state [_] input)
+          (goal? [_ {:keys [pos end]}] (= pos end))
+          (actions [_ {:keys [free pos]}]
+            (for [d [[0 1] [1 0] [0 -1] [-1 0]] :when (free (mapv + pos d))] d))
+          (result [this {:keys [pos] :as state} action]
+            (assoc state :pos (mapv + pos action)))
+          (step-cost [this state action]
+            1))))))
 
-(defn solve-part2 [input])
+(defn manhattan [[a1 a2] [b1 b2]]
+  (+ (abs (- a1 b1)) (abs (- a2 b2))))
+
+(defn cheats [at-least max-cheats input]
+  (let [path (fastest-path input)]
+    (for [i (range (count path))
+          j (range i (count path))
+          :let [distance (manhattan (path i) (path j))
+                saving (- j i distance)]
+          :when (and (<= at-least saving)
+                     (<= 2 distance max-cheats))]
+      saving)))
+
+(defn solve-part1 [input]
+   (count (cheats 100 2 input)))
+
+(defn solve-part2 [input]
+   (count (cheats 100 20 input)))
+
+(comment
+  (time (solve-part1 (parse-input (slurp "resources/day20.txt"))))
+  (time (solve-part2 (parse-input (slurp "resources/day20.txt")))))
--- test/aoc2024/day20_test.clj
+++ test/aoc2024/day20_test.clj
@@ -1,16 +1,30 @@
 (ns aoc2024.day20-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc2024.day20 :refer [parse-input solve-part1 solve-part2]]
+            [aoc2024.day20 :refer [cheats parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 44 (count (cheats 1 2 (parse-input example-input)))))
+    (is (= 285 (count (cheats 50 20 (parse-input example-input))))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day20.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 1384 (solve-part1 input)))
+      (is (= 1008542 (solve-part2 input))))))
