2f8dc19ca2352a0b9788ffed492bedce3564cd94
David Wagner <wagdav@gmail.com>
2024-12-22 12:03:16 +0100

    Solve day22

--- src/aoc2024/day22.clj
+++ src/aoc2024/day22.clj
@@ -1,10 +1,54 @@
 (ns aoc2024.day22
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math :refer [round]]))
 
 (defn parse-input [input]
   (->> (re-seq #"-?\d+" input)
        (mapv parse-long)))
 
-(defn solve-part1 [input])
+(defn mix [n v] (bit-xor n v))
+(defn prune [n] (mod n 16777216)) ; 2^24
 
-(defn solve-part2 [input])
+(defn step [n]
+  (let [p1 (prune (mix n  (* 64 n)))
+        p2 (prune (mix p1 (quot p1 32)))
+        p3 (prune (mix p2 (* p2 2048)))]
+    p3))
+
+(defn solve-part1 [input]
+  (apply + (for [n input] (nth (iterate step n) 2000))))
+
+(defn prices [start]
+  (for [n (iterate step start)]
+    (rem n 10)))
+
+(defn changes [start]
+  (for [[a b] (partition 2 1 (prices start))]
+    (- b a)))
+
+(defn patterns [start]
+  (partition 4 1 (changes start)))
+
+(defn signal [start]
+  (map vector (patterns start)
+              (drop 4 (prices start))))
+
+(defn winning-by-sequence [start]
+  (update-vals (group-by first (take 2000 (signal start)))
+               (comp second first)))
+
+(defn solve-part2 [input]
+  (let [sws (map winning-by-sequence input)
+        all-seqs (reduce
+                   (fn [res s]
+                     (into res (keys s)))
+                   #{}
+                   sws)]
+    (apply max
+      (for [s all-seqs]
+        (apply + (for [sw sws] (get sw s 0)))))))
+
+(comment
+  (time (solve-part1 (parse-input (slurp "resources/day22.txt"))))
+  (time (solve-part2 (parse-input (slurp "resources/day22.txt")))))
+
--- test/aoc2024/day22_test.clj
+++ test/aoc2024/day22_test.clj
@@ -3,14 +3,22 @@
             [aoc2024.day22 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "1
+10
+100
+2024")
+
+(def example-input2 "1
+2
+3
+2024")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 37327623 (solve-part1 (parse-input example-input))))
+    (is (= 23 (solve-part2 (parse-input example-input2)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day22.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 14082561342 (solve-part1 input))))))
+      ;(is (= 1568 (solve-part2 input)))))) ; SLOW
