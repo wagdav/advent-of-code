bd268d73db8675e874e8611a1c2d7521ba6e1d5a
David Wagner <wagdav@gmail.com>
2024-12-23 07:20:16 +0100

    Solve day23

--- src/aoc2024/day23.clj
+++ src/aoc2024/day23.clj
@@ -2,9 +2,60 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [pairs (for [line (str/split-lines input)]
+                (str/split line #"-"))]
+    (merge-with into
+                (update-vals (group-by first pairs)
+                             #(set (map second %)))
+                (update-vals (group-by first (for [[a b] pairs] [b a]))
+                             #(set (map second %))))))
 
-(defn solve-part1 [input])
+(defn connected? [g a b]
+  (contains? (g a #{}) b))
 
-(defn solve-part2 [input])
+(defn triples [g]
+  (set (for [a (keys g)
+             b (keys g)
+             c (keys g)
+             :when (and (or (str/starts-with? a "t")
+                            (str/starts-with? b "t")
+                            (str/starts-with? c "t"))
+                        (and (connected? g a b)
+                             (connected? g b c)
+                             (connected? g c a)))]
+
+         (set [a b c]))))
+
+(defn filter-connected [g ps]
+  (reduce
+    (fn [res p]
+      (if (every? #(connected? g p %) res)
+        (conj res p)
+        res))
+    []
+    ps))
+
+(defn network [g start]
+  (loop [frontier #{start}, visited #{start}]
+    (if (seq frontier)
+      (let [frontier' (->> (for [f frontier
+                                 p (g f)
+                                 :when (and (not (visited p))
+                                            (every? #(connected? g p %) visited))]
+                             p)
+                           (filter-connected g))]
+        (recur (set frontier') (into visited frontier')))
+      visited)))
+
+(defn solve-part1 [input]
+  (count (triples input)))
+
+(defn solve-part2 [input]
+  (->> (for [n (keys input)]
+         (network input n))
+       (apply max-key count)
+       (str/join ",")))
+
+(comment
+  (time (solve-part1 (parse-input (slurp "resources/day23.txt"))))
+  (solve-part2 (parse-input (slurp "resources/day23.txt"))))
--- test/aoc2024/day23_test.clj
+++ test/aoc2024/day23_test.clj
@@ -3,14 +3,45 @@
             [aoc2024.day23 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 7 (solve-part1 (parse-input example-input))))
+    (is (= "co,de,ka,ta" (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day23.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      ;(is (= 1485 (solve-part1 input))) ; SLOW
+      (= "cc,dz,ea,hj,if,it,kf,qo,sk,ug,ut,uv,wh" (solve-part2 input)))))
