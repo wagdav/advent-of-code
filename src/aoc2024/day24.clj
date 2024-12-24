329a3196ac6ebbbdee48888961c52b244854fd0e
David Wagner <wagdav@gmail.com>
2024-12-24 07:57:48 +0100

    Solve day24

--- src/aoc2024/day24.clj
+++ src/aoc2024/day24.clj
@@ -2,9 +2,79 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [[env ops] (str/split input #"\R\R")]
+    {:env (into {} (for [[wire value] (map #(str/split % #": ") (str/split-lines env))]
+                     [wire (if (= value "1") true false)]))
+     :ops (for [[a op b _ res] (->> ops (str/split-lines) (map #(str/split % #" ")))]
+            [a op b res])}))
 
-(defn solve-part1 [input])
+(defn z-wires [ops]
+  (->> ops
+       (mapcat (fn [[a op b res]] [a b res]))
+       set
+       (filter #(str/starts-with? % "z"))))
+
+(defn pass [{:keys [env ops]}]
+  (reduce
+    (fn [env [a op b dst]]
+      (if (and (some? (env a)) (some? (env b)))
+         (assoc env dst (case op
+                          "AND" (and (env a) (env b))
+                          "OR"  (or (env a) (env b))
+                          "XOR" (distinct? (env a) (env b))))
+         env))
+    env
+    ops))
+
+(defn run [{:keys [env ops]}]
+  (let [zs (z-wires ops)]
+    (loop [env env]
+      (if (every? #(some? (env %)) zs)
+        env
+        (recur (pass {:env env :ops ops}))))))
+
+(defn to-decimal [bits]
+  (Long/parseLong (str/join bits) 2))
+
+(defn solve-part1 [input]
+  (let [env (run input)]
+    (to-decimal (for [k (reverse (sort (keys env)))
+                      :when (str/starts-with? k "z")]
+                  (if (env k) 1 0)))))
+
+(defn graphviz [{:keys [ops]}]
+  (println "digraph D {")
+  (println " ordering=out;")
+  (println "  subgraph cluster_z  {" (str/join "; " (for [i (range 45)] (format "z%02d" i))) "}")
+  (doseq [[a op b res] ops]
+    (let [res' (case res
+                  "z27" "kcd"
+                  "kcd" "z27"
+
+                  "z23" "pfn"
+                  "pfn" "z23"
+
+                  "wkb" "tpk"
+                  "tpk" "wkb"
+
+                  "z07" "shj"
+                  "shj" "z07"
+                  res)]
+      (when-not (str/starts-with? res' "z")
+          (println " " res' (format "[label=%s%s];" res', op)))
+      (println " " a "-> " res')
+      (println " " b "-> " res')))
+  (println "}"))
+
 
 (defn solve-part2 [input])
+; Solved visually. I generated a Graphviz diagram, displayed it with
+;
+;   xdot -f circo day24.dot
+;
+; Then, I looked for nodes which caused irregularities.
+
+(comment
+  (do
+    (binding [*out* (clojure.java.io/writer "day24.dot")]
+      (graphviz (parse-input (slurp "resources/day24.txt"))))))
--- test/aoc2024/day24_test.clj
+++ test/aoc2024/day24_test.clj
@@ -3,14 +3,60 @@
             [aoc2024.day24 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "x00: 1
+x01: 0
+x02: 1
+x03: 1
+x04: 0
+y00: 1
+y01: 1
+y02: 1
+y03: 1
+y04: 1
+
+ntg XOR fgs -> mjb
+y02 OR x01 -> tnw
+kwq OR kpj -> z05
+x00 OR x03 -> fst
+tgd XOR rvg -> z01
+vdt OR tnw -> bfw
+bfw AND frj -> z10
+ffh OR nrd -> bqk
+y00 AND y03 -> djm
+y03 OR y00 -> psh
+bqk OR frj -> z08
+tnw OR fst -> frj
+gnj AND tgd -> z11
+bfw XOR mjb -> z00
+x03 OR x00 -> vdt
+gnj AND wpb -> z02
+x04 AND y00 -> kjc
+djm OR pbm -> qhw
+nrd AND vdt -> hwm
+kjc AND fst -> rvg
+y04 OR y02 -> fgs
+y01 AND x02 -> pbm
+ntg OR kjc -> kwq
+psh XOR fgs -> tgd
+qhw XOR tgd -> z09
+pbm OR djm -> kpj
+x03 XOR y03 -> ffh
+x00 XOR y04 -> ntg
+bfw OR bqk -> z06
+nrd XOR fgs -> wpb
+frj XOR qhw -> z04
+bqk OR frj -> z07
+y03 OR x01 -> nrd
+hwm AND bqk -> z03
+tgd XOR rvg -> z12
+tnw OR pbm -> gnj")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
+    (is (= 2024 (solve-part1 (parse-input example-input))))
     (is (nil? (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day24.txt")))]
-      (is (nil? (solve-part1 input)))
+      (is (= 57588078076750 (solve-part1 input)))
       (is (nil? (solve-part2 input))))))
