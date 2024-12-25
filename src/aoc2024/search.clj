8f85e01f7643b214baa7de804015e31c70ba96fd
David Wagner <wagdav@gmail.com>
2024-12-25 12:37:01 +0100

    search: add variant that returns all shortest paths

--- src/aoc2024/search.clj
+++ src/aoc2024/search.clj
@@ -1,5 +1,5 @@
 (ns aoc2024.search
-  (:require [clojure.data.priority-map :refer [priority-map-keyfn]]))
+  (:require [clojure.data.priority-map :refer [priority-map priority-map-keyfn]]))
 
 (defprotocol Problem
   (actions [this state])
@@ -11,6 +11,9 @@
 ; Russel&Norvig AI (Section 3.1.1 p 78.)
 (defrecord Node [state actions path path-cost])
 
+(defn make-node [state]
+  (map->Node {:state state :actions [] :path [state] :path-cost 0}))
+
 (defn child-node [problem parent action]
   (let [state (result problem (:state parent) action)]
     (map->Node
@@ -19,6 +22,10 @@
        :path (conj (:path parent) state)
        :path-cost (+ (:path-cost parent)
                      (step-cost problem (:state parent) action))})))
+
+(defn- expand [problem node]
+  (for [action (actions problem (:state node))]
+    (child-node problem node action)))
 
 (defn uniform-cost [problem]
   (let [start (initial-state problem)]
@@ -35,6 +42,24 @@
                                        :let [c (child-node problem node action)
                                              s (:state c)]
                                        :when (not (explored s))] [s c]))))))))
+
+(defn uniform-cost-all [problem]
+  (let [start (make-node (initial-state problem))]
+    (loop [reached {}
+           frontier (priority-map start (:path-cost start))]
+      (when-let [[node cost] (peek frontier)]
+        (if (goal? problem (:state node))
+          (->> frontier
+               (filter (fn [[n c]] (and (= cost c) (goal? problem (:state n)))))
+               (map first))
+          (let [children (for [child (expand problem node)
+                               :let [s (:state child)
+                                     c (:path-cost child)]
+                               :when (or (not (reached s)) (<= c (reached s)))]
+                           child)]
+            (recur
+              (into reached (for [c children] [(:state c) (:path-cost c)]))
+              (into (pop frontier) (for [c children] [c (:path-cost c)])))))))))
 
 (defn best-first [problem f]
   (let [start-state (initial-state problem)
