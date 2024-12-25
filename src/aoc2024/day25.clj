5644e454811c4bad17d9d24233c7dfade96b31f5
David Wagner <wagdav@gmail.com>
2024-12-25 09:56:02 +0100

    Solve day25

--- src/aoc2024/day25.clj
+++ src/aoc2024/day25.clj
@@ -2,9 +2,33 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (let [ss (->> (for [p (str/split input #"\R\R")]
+                  (str/split-lines p))
+                (group-by #(= "#####" (first %))))]
+    {:locks (->> (ss true))
+     :keys (ss false)}))
 
-(defn solve-part1 [input])
+(defn transpose [v]
+  (apply mapv vector v))
 
-(defn solve-part2 [input])
+(defn lock->heights [lock]
+  (->> (drop 1 lock)
+       (transpose)
+       (map #(filter #{\#} %))
+       (map count)))
+
+(defn key->heights [k]
+  (->> (butlast k)
+       (transpose)
+       (map #(filter #{\#} %))
+       (map count)))
+
+(defn solve-part1 [{:keys [keys locks]}]
+  (count
+    (for [l locks
+          k keys
+          :let [lh (lock->heights l)
+                kh (key->heights k)
+                m (apply max (mapv + lh kh))]
+          :when (<= m 5)]
+      m)))
--- test/aoc2024/day25_test.clj
+++ test/aoc2024/day25_test.clj
@@ -1,16 +1,53 @@
 (ns aoc2024.day25-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc2024.day25 :refer [parse-input solve-part1 solve-part2]]
+            [aoc2024.day25 :refer [parse-input solve-part1]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####")
+
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 3 (solve-part1 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day25.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 3508 (solve-part1 input))))))
