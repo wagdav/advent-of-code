fa02880b1dad03eb0b7f03e91fd9b0c28a90edfe
David Wagner <wagdav@gmail.com>
2025-12-08 11:23:06 +0100

    day 8 part 1

---
+++ src/aoc2025/day08.clj
@@ -0,0 +1,77 @@
+(ns aoc2025.day08
+  (:require [clojure.string :as str]))
+
+(def example-input "162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689")
+
+(defn parse-input [input]
+  (->> input
+       (re-seq #"\d+")
+       (map parse-long)
+       (partitionv 3)))
+
+(defn distance [[a1 a2 a3] [b1 b2 b3]]
+  (+ (* (- a1 b1) (- a1 b1))
+     (* (- a2 b2) (- a2 b2))
+     (* (- a3 b3) (- a3 b3))))
+
+(defn circuit [start connections]
+  (loop [explored #{start}
+         frontier [start]]
+    (if (empty? frontier)
+      explored
+      (let [f (peek frontier)
+            to (->> connections
+                    (filter #(= f (first %)))
+                    (map second))
+            from (->> connections
+                      (filter #(= f (second %)))
+                      (map first))]
+        (recur
+          (-> explored
+              (into to)
+              (into from))
+          (-> (pop frontier)
+              (into (remove explored from))
+              (into (remove explored to))))))))
+
+(defn solve-part1 [n-pairs input]
+  (let [by-distance (sort-by first
+                     (for [[i a] (map-indexed vector input)
+                           [j b] (map-indexed vector input)
+                           :when (< i j)]
+                       [(distance a b) a b]))
+        connections (->> (take n-pairs by-distance)
+                         (mapv #(drop 1 %)))]
+    (loop [circuits []
+           junctions (set input)]
+      (if (empty? junctions)
+        (apply * (take 3 (sort > (map count circuits))))
+        (let [new-circuit (circuit (first junctions) connections)]
+           (recur
+             (conj circuits new-circuit)
+             (remove new-circuit (rest junctions))))))))
+
+(defn solve-part2 [id-ranges])
+
+(->> example-input
+     parse-input
+     (solve-part1 10))
---
+++ test/aoc2025/day08_test.clj
@@ -0,0 +1,36 @@
+(ns aoc2025.day08-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [aoc2025.day08 :refer [parse-input solve-part1 solve-part2]]
+            [clojure.java.io :as io]))
+
+(def example-input "162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689")
+
+(deftest works
+  (testing "with example input"
+    (is (= 40 (solve-part1 10 (parse-input example-input))))
+    (is (= nil (solve-part2 (parse-input example-input)))))
+
+  (testing "with real input"
+    (let [input (parse-input (slurp (io/resource "day08.txt")))]
+      (is (= 32103 (solve-part1 1000 input)))
+      (is (= nil (solve-part2 input))))))
+
