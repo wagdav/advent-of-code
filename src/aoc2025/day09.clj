a86992612a66e539a58f68fd70764b48df30584d
David Wagner <wagdav@gmail.com>
2025-12-09 08:29:19 +0100

    Solve 2025 day09

--- src/aoc2025/day09.clj
+++ src/aoc2025/day09.clj
@@ -2,9 +2,46 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (->> input
+       (re-seq #"\d+")
+       (map parse-long)
+       (partition 2)))
 
-(defn solve-part1 [input])
+(defn area [[x1 y1] [x2 y2]]
+  (* (inc (abs (- x1 x2)))
+     (inc (abs (- y1 y2)))))
 
-(defn solve-part2 [input])
+(defn edge-outside-rectangle? [[[r1 c1] [r2 c2]] [[x1 y1] [x2 y2]]]
+  (let [[x1 x2] (sort [x1 x2])
+        [y1 y2] (sort [y1 y2])
+        [r1 r2] (sort [r1 r2])
+        [c1 c2] (sort [c1 c2])]
+    (if (= r1 r2)
+      ; horizontal edge
+      (or
+        (or (<= r1 x1)  ; row outside the rectangle
+            (<= x2 r2))
+        (or (<= c1 c2 y1)
+            (<= y2 c1 c2)))
+      ; vertical dege
+      (or
+        (or (<= c1 y1)  ; col outside the rectangle
+            (<= y2 c2))
+        (or (<= r1 r2 x1)
+            (<= x2 r1 r2))))))
+
+(defn solve-part1 [input]
+  (apply max
+    (for [[i a] (map-indexed vector input)
+          [j b] (map-indexed vector input)
+          :when (< i j)]
+      (area a b))))
+
+(defn solve-part2 [input]
+  (let [edges (partitionv 2 1 input input)]
+    (apply max
+      (for [[i a] (map-indexed vector input)
+            [j b] (map-indexed vector input)
+            :when (and (< i j)
+                       (every? #(edge-outside-rectangle? % [a b]) edges))]
+        (area a b)))))
--- test/aoc2025/day09_test.clj
+++ test/aoc2025/day09_test.clj
@@ -3,14 +3,21 @@
             [aoc2025.day09 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 50 (solve-part1 (parse-input example-input))))
+    (is (= 24 (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day09.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (is (= 4755429952 (solve-part1 input)))
+      (is (= 1429596008 (solve-part2 input))))))
