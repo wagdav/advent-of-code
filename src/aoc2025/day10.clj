da75dc31f6d0b2f30c76f47496dd8e8595133bf1
David Wagner <wagdav@gmail.com>
2025-12-10 09:19:58 +0100

    2025 day10 part1

--- src/aoc2025/day10.clj
+++ src/aoc2025/day10.clj
@@ -1,10 +1,58 @@
 (ns aoc2025.day10
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [aoc2024.search :as search]))
+
+(def example-input "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}")
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (->> (str/split-lines input)
+       (map (fn [line]
+              (let [parts (str/split line #" ")]
+                {:indicators (->> (first parts)
+                                  (keep #{\. \#})
+                                  (mapv {\. false \# true}))
+                 :buttons (->> (butlast parts)
+                               (drop 1)
+                               (map #(re-seq #"\d+" %))
+                               (map #(mapv parse-long %))
+                               (map set))
+                 :joltages (->> (last parts)
+                                (re-seq #"\d+")
+                                (mapv parse-long))})))))
 
-(defn solve-part1 [input])
+(defn toggle [state buttons]
+  (vec
+    (map-indexed (fn [i s]
+                   (if (buttons i)
+                     (not s)
+                     s))
+                 state)))
+
+(toggle [false false] #{0})
+
+(defn fewest-presses [{:keys [indicators buttons]}]
+  (first
+    (map :path-cost
+      (search/uniform-cost-all
+        (reify search/Problem
+          (actions [this state]
+            buttons)
+          (goal? [this state]
+            (= state indicators))
+          (initial-state [this]
+            (repeat (count indicators) false))
+          (result [this state action]
+            (toggle state action))
+          (step-cost [this state action]
+            1))))))
+
+(defn solve-part1 [input]
+  (apply + (map fewest-presses input)))
+
+(->> example-input
+     parse-input
+     solve-part1)
 
 (defn solve-part2 [input])
--- test/aoc2025/day10_test.clj
+++ test/aoc2025/day10_test.clj
@@ -3,14 +3,16 @@
             [aoc2025.day10 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
+    (is (= 7 (solve-part1 (parse-input example-input))))
     (is (nil? (solve-part2 (parse-input example-input)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day10.txt")))]
-      (is (nil? (solve-part1 input)))
+      (is (= 512 (solve-part1 input)))
       (is (nil? (solve-part2 input))))))
