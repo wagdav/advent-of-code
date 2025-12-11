81d27873b9bb080fb75d9a86ca658a4487c4af92
David Wagner <wagdav@gmail.com>
2025-12-11 09:02:06 +0100

    Solve 2025 day11

--- src/aoc2025/day11.clj
+++ src/aoc2025/day11.clj
@@ -2,9 +2,72 @@
   (:require [clojure.string :as str]))
 
 (defn parse-input [input]
-  (->> (re-seq #"-?\d+" input)
-       (mapv parse-long)))
+  (->> input
+       str/split-lines
+       (map #(str/split % #"[ :]+"))
+       (reduce #(assoc %1 (first %2) (rest %2)) {})))
 
-(defn solve-part1 [input])
+(defn paths
+  ([input from to]
+   (paths input from to nil))
+  ([input from to via]
+   (loop [paths #{}, q [[from]], explored #{}]
+     (if (empty? q)
+       (count paths)
+       (let [cur (peek q)
+             q' (into (pop q)
+                      (->> (input (last cur))
+                           (remove explored)
+                           (map #(conj cur %))))]
+         (recur
+           (if (and (= to (last cur))
+                    (if via ((set cur) via) true))
+             (conj paths cur)
+             paths)
+           q'
+           (conj explored cur)))))))
 
-(defn solve-part2 [input])
+(defn solve-part1 [input]
+  (paths input "you" "out"))
+
+(defn solve-part2 [input]
+  (let [start "svr"
+        end "out"
+        ; I plotted the input data with Graphviz which revealed that the
+        ; devices are connected through layers of "hubs".  All paths must go
+        ; through through one hub in each layer. I identified the hubs
+        ; visually.
+        hubs [["yad" "caw" "jub" "aek" "xvt"]   ;0
+              ["xxt" "bmm" "nxq" "nhk"]         ;1  via "fft"
+              ["arh" "sxr", "els" "tku" "zgf"]  ;2
+              ["xdu" "fbt" "tak" "xvf"]         ;3
+              ["sus" "tgd" "you"]]]             ;4  via "dac"
+   (apply +
+     (for [h0 (hubs 0)
+           h1 (hubs 1)
+           h2 (hubs 2)
+           h3 (hubs 3)
+           h4 (hubs 4)]
+       (*
+         (paths (apply dissoc input (hubs 0)) start h0)
+         (paths (apply dissoc input (hubs 1)) h0 h1 "fft")
+         (paths (apply dissoc input (hubs 2)) h1 h2)
+         (paths (apply dissoc input (hubs 3)) h2 h3)
+         (paths (apply dissoc input (hubs 4)) h3 h4 "dac")
+         (paths input h4 end))))))
+
+(defn dot! [input]
+  (println "digraph G {")
+  (doseq [line (into (for [[from tos] input
+                           to tos]
+                      (str " " from " -> " to ";")))]
+    (println line))
+  (println "}"))
+
+(defn run [opts]
+  (prn (solve-part2 (parse-input (slurp (clojure.java.io/resource "day11.txt"))))))
+
+; clj -X aoc2025.day11/dot
+(defn dot [opts]
+  (->> (parse-input (slurp (clojure.java.io/resource "day11.txt")))
+       dot!))
--- test/aoc2025/day11_test.clj
+++ test/aoc2025/day11_test.clj
@@ -3,14 +3,37 @@
             [aoc2025.day11 :refer [parse-input solve-part1 solve-part2]]
             [clojure.java.io :as io]))
 
-(def example-input "")
+(def example-input "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out")
+
+(def example-input2 "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out")
 
 (deftest works
   (testing "with example input"
-    (is (nil? (solve-part1 (parse-input example-input))))
-    (is (nil? (solve-part2 (parse-input example-input)))))
+    (is (= 5 (solve-part1 (parse-input example-input))))
+    (is (= 2 (solve-part2 (parse-input example-input2)))))
 
   (testing "with real input"
     (let [input (parse-input (slurp (io/resource "day11.txt")))]
-      (is (nil? (solve-part1 input)))
-      (is (nil? (solve-part2 input))))))
+      (time (is (= 566 (solve-part1 input))))
+      (time (is (= 331837854931968 (solve-part2 input)))))))
